e19b241ed609446c2b6de375f0dab3432a2ef783
Felix Chin <felixmchin@gmail.com>
2020-07-20 16:23:04 -0700

    finished feature

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import ViewCards from './view-cards';
+import ReviewCards from './review-cards';
+import CreateCard from './create-card';
+import Nav from './nav'
+
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { view: 'view-cards' }
+    this.setView = this.setView.bind(this);
+  }
+
+  setView(newView) {
+    this.setState({ view: newView })
+  }
+
+  getView() {
+    switch (this.state.view) {
+      case 'create-card':
+        return <CreateCard />;
+      case 'review-cards':
+        return <ReviewCards />;
+      case 'view-cards':
+        return <ViewCards />;
+      default:
+        return null;
+    }
+  }
+
   render() {
     return (
-      <h1 className="text-center">Flash Card App</h1>
-    )
+      <div>
+        <Nav setView={this.setView} view={this.state.view}/>
+        { this.getView() }
+      </div>
+    );
   }
 }
---
+++ src/components/create-card.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateCard(props) {
+  return (
+    <h1 className="text-center">Create New Card</h1>
+  )
+}
