b7a1fdc8c2857b0eab0ff89f651d183a9a47ff5f
Felix Chin <felixmchin@gmail.com>
2020-09-29 11:42:13 -0700

    added heading to navbar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -29,9 +29,14 @@ export default function Nav(props) {
 
   return (
     <nav className="navbar d-flex justify-content-end mb-4">
-      <span role="button" onClick={() => setView('view-cards')} className={'btn btn-outline-primary border-0 ' + viewClass}>View Cards</span>
-      <span role="button" onClick={handleReviewClick} className={'btn btn-outline-primary border-0 ml-1 ' + reviewClass}>Review</span>
-      <span role="button" onClick={() => setView('create-card')} className={'btn btn-outline-primary border-0 ml-1 ' + createClass}>Create Card</span>
+      <div className="navbar-brand">
+        <h4 onClick={() => setView('create-card')} className="font-weight-bold">Simple Flashcards</h4>
+      </div>
+      <div className="ml-auto">
+        <span role="button" onClick={() => setView('view-cards')} className={'btn btn-outline-primary border-0 ' + viewClass}>View Cards</span>
+        <span role="button" onClick={handleReviewClick} className={'btn btn-outline-primary border-0 ml-1 ' + reviewClass}>Review</span>
+        <span role="button" onClick={() => setView('create-card')} className={'btn btn-outline-primary border-0 ml-1 ' + createClass}>Create Card</span>
+      </div>
     </nav>
   )
 }
